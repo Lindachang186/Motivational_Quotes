663494dd6a2a5c223f8d73d1d65f382827935082
Linda Chang <lindachang186@gmail.com>
2019-03-03 02:17:44 +0000

    modified log in page and added actions to links

--- app/controllers/application_controlller.rb
+++ app/controllers/application_controlller.rb
@@ -9,26 +9,16 @@ class ApplicationController < Sinatra::Base
     set :session_secret, "golfclubsaregreat"
   end
 
-
   get '/' do
     erb :index
   end
 
-  helpers do
-    def redirect_if_not_logged_in
-      if !logged_in?
-        redirect "/login?error=You have to be logged in to do that"
-      end
-    end
+  get '/signup' do
+    erb :new
+  end
 
-    def logged_in?
-      !!session[:user_id]
-    end
-
-    def current_user
-      User.find(session[:user_id])
-    end
-
+  get '/login' do
+    erb :login
   end
 
 
