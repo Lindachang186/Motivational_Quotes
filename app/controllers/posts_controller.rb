3608f6f089b7ff0e732788faefa1819bf96895e0
Linda Chang <lindachang186@gmail.com>
2019-03-10 05:45:46 +0000

    created posts and allowed users to view their own posts!

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,9 +3,23 @@ require './config/environment'
 class PostsController < ApplicationController
 
  get '/posts/index' do
-   @post = Post.all
+   @posts = Post.all
    erb :'posts/index'
  end
+
+ get '/posts/new' do
+   erb :'posts/new'
+ end
+
+ post '/posts/index' do
+   @posts = Post.create(:title => params[:title], :content => params[:content])
+   redirect to "/posts/#{@posts.id}"
+end
+
+  get '/posts/:id' do
+    @posts = Post.find_by_id(params[:id])
+    erb :'posts/show'
+  end
 
 
 end
