ee53c352e3dd4fd4db98a944894f942d3c09a151
Linda Chang <lindachang186@gmail.com>
2019-03-14 17:12:32 +0000

    added password secret heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,10 @@ require './config/environment'
 class ApplicationController < Sinatra::Base
 
   configure do
-    set :public_folder, 'public'
-    set :views, 'app/views'
-    enable :sessions
-    set :session_secret, "golfclubsaregreat"
+     set :public_folder, 'public'
+     set :views, 'app/views'
+     enable :sessions
+     set :session_secret, ENV.fetch('SESSION_SECRET')
   end
 
   get '/' do
