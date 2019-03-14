4988ce5b63fe55c3f2ff2a39624046ab1f852561
Linda Chang <lindachang186@gmail.com>
2019-03-14 17:52:02 +0000

    corrected

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,10 @@ require './config/environment'
 class ApplicationController < Sinatra::Base
 
   configure do
-     set :public_folder, 'public'
-     set :views, 'app/views'
-     enable :sessions
-     set :session_secret, ENV.fetch('SESSION_SECRET')
+    set :public_folder, 'public'
+   set :views, 'app/views'
+   enable :sessions
+     set :session_secret, "motivational_quotes"
   end
 
   get '/' do
